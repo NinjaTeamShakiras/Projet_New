b5c925d09700f6363115e8b03c970075c0a087b8
Rouxfoul <thomas.georget.reglin@gmail.com>
2017-05-09 14:14:35 +0200

    Recup v1 Thomas

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -17,17 +17,50 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
+		//On fait appel au model Utilisateur pour trouver l'utilisateur en cours
+		//--> On le trouve grace à son login
+		$user = Utilisateur::model()->findByAttributes(array('login'=>$this->username));
+
+		if($user == null)
+		{
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
+		}
+		else if($this->password != $user->mot_de_passe)
+		{
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
+		}
 		else
+		{
 			$this->errorCode=self::ERROR_NONE;
+
+			//Gestion des rôles
+			//--> Si c'est un employé, on lui donne le rôle d'employé
+			if($user->role == "employe")
+			{
+				$this->setState('type', 'employe');	
+				$this->setState('role', 'employe');
+			}
+
+			//--> Si c'est une entreprise, on lui donne le rôle d'entreprise
+			else if($user->role == "entreprise")
+			{
+				$this->setState('type', 'entreprise');	
+				$this->setState('role', 'entreprise');
+			}
+
+			//--> Si c'est un admin, on lui donne le rôle d'admin
+			else if($user->role == "admin")
+			{
+				$this->setState('type', 'admin');	
+				$this->setState('role', 'admin');
+			}
+
+			date_default_timezone_set('Europe/Paris');
+			$date = (new \DateTime())->format('Y-m-d H:i:s');
+			$user->date_derniere_connexion = $date;
+			$user->save();
+		}	
+
 		return !$this->errorCode;
 	}
 }
