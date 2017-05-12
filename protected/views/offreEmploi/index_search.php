f8cb85ec30e55a2c36a039b63d46ea1a16c4ea85
culeren <kcnarfy-m.73@hotmail.fr>
2017-05-12 12:04:11 +0200

    page de résultat de recherche peut eqgualement faire une recherche

--- protected/views/offreEmploi/index_search.php
+++ protected/views/offreEmploi/index_search.php
@@ -10,10 +10,150 @@ $this->breadcrumbs=array(
 
 	$this->menu=array(
 		array('label'=>'Liste des offres d\'emplois', 'url'=>array('/offreEmploi/index')), // Voir toutes les offres d'emplois
-		array('label'=>'Nouvelle recherche d\'offres d\'emplois', 'url'=>array('/offreEmploi/recherche')), // Rechercher des offres d'emplois
+		//array('label'=>'Nouvelle recherche d\'offres d\'emplois', 'url'=>array('/offreEmploi/recherche')), // Rechercher des offres d'emplois
 	);
 
 ?>
+
+
+
+<h1>Nouvelle recherche : </h1>
+
+<?php
+	$login = Yii::app()->user->getId();
+	// Récupération de l'utilisateur
+	$utilisateur = Utilisateur::model()->FindByAttributes(array("mail"=>$login));
+	$model = OffreEmploi::model();
+	// Récupération de toutes les offres
+	$tabOffre = OffreEmploi::model()->FindAll();
+	// Récupération de toutes les entreprises
+	$tabEntreprise = entreprise::model()->FindAll();
+
+	$entreprise = entreprise::model();
+	$adresse = adresse::model();
+
+	// Nombre d'offre total
+	$nombreOffre = sizeof($tabOffre); 
+
+	print("<p> Trouver les offres qui vous correspondent parmis ".$nombreOffre." offres.</p>");
+?>
+
+<div class="wide form">
+
+	<?php
+		//Début du form
+		$form=$this->beginWidget('CActiveForm',
+			array(
+				'action'=>Yii::app()->createUrl('offreEmploi/Search'),
+			)
+		);
+	?>
+
+	<div class="row">
+		<!-- Recherche d'un poste (textfield + dropdownlist+ bouton submit) -->	
+		<?php
+			//Recherche par POSTE
+			echo $form->textField(
+				$model,'poste_offre_emploi', array(	
+					'class' => 'autocomplete-find-offreEmploi',
+					'url_data_auto' => Yii::app()->createUrl('offreEmploi/GetAllPosteJSON'),
+					'size' => 45,
+					'maxlength' => 30,
+					'placeholder' => 'Rechercher par poste',
+				)
+			);
+		?>
+	</div>
+	
+	<div class="row">	
+		<?php
+			//Recherche par TYPE DE CONTRAT (liste déroulante)
+			//-->On ajoute l'option "Sélectionner pour la liste"
+			$static_type = array('' => Yii::t('', 'Sélectionner...'));
+			$typeOffre = CHtml::listData($tabOffre,'type_offre_emploi', 'type_offre_emploi'); // On récupère tout les type d'offre existant
+			echo $form->dropDownList($model,'type_offre_emploi',$static_type + $typeOffre); // On affiche une liste déroulante de toutes les offres
+		?>
+	</div>
+	
+	<div class="row">
+		<?php
+			//Recherche par LIEU
+			echo $form->textField(
+				$adresse,'ville', array(	
+					'class' => 'autocomplete-find-offreEmploi',
+					'url_data_auto' => Yii::app()->createUrl('offreEmploi/GetAllLieuJSON'),
+					'size' => 45,
+					'maxlength' => 30,
+					'placeholder' => 'Rechercher par lieu',
+				)
+			);
+		?>
+	</div>
+	
+	<div class="row">	
+		<?php
+			//Recherche par SECTEUR
+			//-->On ajoute l'option "Sélectionner pour la liste"
+			$static_secteur = array('' => Yii::t('', 'Sélectionner...'));
+			$secteurOffre = CHtml::listData($tabEntreprise,'secteur_activite_entreprise', 'secteur_activite_entreprise'); // On récupère tout les secteur d'offre existant
+			echo $form->dropDownList($entreprise,'secteur_activite_entreprise',$static_secteur + $secteurOffre); // On affiche une liste déroulante de tout les secteur d'activité
+		?>
+	</div>
+	
+	<div class="row buttons">	
+		<?php
+			// Button d'envoi
+			echo CHtml::submitButton('Rechercher');
+		?>
+	</div>
+
+	<?php $this->endWidget(); ?>
+
+	</div>
+
+
+
+	<!-- Autocomplétion : -->
+
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+
+<script type="text/javascript">
+	var autocomplete_class = ".autocomplete-find-offreEmploi"; 
+	var url_data = $(autocomplete_class).attr('url_data_auto');
+
+	$.ajax({
+		type: "POST",
+		dataType: "text",
+		url: url_data,
+		data: {  },
+		success: function( response ) 
+	    {
+			autocomplete( response );
+		},
+		error: function( error )
+		{
+			console.log("Error récupération des données");
+		}
+	});
+
+	function autocomplete( test )
+	{
+		var list_json = JSON.parse( test.substring(1, test.length) );
+		$( autocomplete_class ).autocomplete({
+		      source: list_json
+	    });
+	}
+
+</script>
+
+
+
+
+
+
+
 
 <h1>Résultat de votre recherche : </h1>
 
