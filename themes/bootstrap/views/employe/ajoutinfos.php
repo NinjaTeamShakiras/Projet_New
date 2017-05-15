c88fde049beeebedbd7a08c088be910eedffd662
vanhovea <alexis.vanhove@etu.univ-savoie.fr>
2017-05-15 13:56:52 +0200

    Suite du design

--- themes/bootstrap/views/employe/ajoutinfos.php
+++ themes/bootstrap/views/employe/ajoutinfos.php
@@ -20,38 +20,38 @@
 			),
 		));
 		?>
-		<div class="rat" id="nom-formation">
+		<div class="row div-field" id="nom-formation">
 			<?php echo $form->labelEx($formation,'intitule_formation'); ?>
 			<?php echo $form->textfield($formation,'intitule_formation',array('class'=>'champ-input')); ?>
 			<?php echo $form->error($formation,'intitule_formation'); ?>
 		</div>
 
 
-		<div class="rat" id="lieu-formation">
+		<div class="row div-field" id="lieu-formation">
 			<?php echo $form->labelEx($formation,'etablissement_formation'); ?>
 			<?php echo $form->textfield($formation,'etablissement_formation',array('class'=>'champ-input')); ?>
 			<?php echo $form->error($formation,'etablissement_formation'); ?>
 		</div>
 
-		<div class="rat" id="diplome-obt">
+		<div class="row div-field" id="diplome-obt">
 			<?php echo $form->labelEx($formation,'diplome_formation'); ?>
 			<?php echo $form->textfield($formation,'diplome_formation',array('class'=>'champ-input')); ?>
 			<?php echo $form->error($formation,'diplome_formation'); ?>
 		</div>
 
-		<div class="rat" id="debut-formation">
+		<div class="row div-field" id="debut-formation">
 			<?php echo $form->labelEx($formation,'date_debut_formation'); ?>
 			<?php echo $form->textfield($formation,'date_debut_formation',array('class'=>'champ-input')); ?>
 			<?php echo $form->error($formation,'date_debut_formation'); ?>
 		</div>
 
-		<div class="rat" id="fin-formation">
+		<div class="row div-field" id="fin-formation">
 			<?php echo $form->labelEx($formation,'date_fin_formation'); ?>
 			<?php echo $form->textfield($formation,'date_fin_formation',array('class'=>'champ-input')); ?>
 			<?php echo $form->error($formation,'date_fin_formation'); ?>
 		</div>
 
-		<div class="rat" id="description-formation">
+		<div class="row div-field" id="description-formation">
 			<?php echo $form->labelEx($formation,'description_formation'); ?>
 			<?php echo $form->textarea($formation,'description_formation',array('class'=>'champ-input champ-description')); ?>
 			<?php echo $form->error($formation,'description_formation'); ?>
@@ -59,32 +59,32 @@
 
 		<h1 class='intitule'>Experiences Professionnelles</h1>
 
-		<div class="rat" id="intitule-experience">
+		<div class="row div-field" id="intitule-experience">
 			<?php echo $form->labelEx($experiencePro,'intitule_experience'); ?>
 			<?php echo $form->textfield($experiencePro,'intitule_experience',array('class'=>'champ-input')); ?>
 			<?php echo $form->error($experiencePro,'intitule_experience'); ?>
 		</div>
 
 
-		<div class="rat" id="entreprise-experience">
+		<div class="row div-field" id="entreprise-experience">
 			<?php echo $form->labelEx($experiencePro,'entreprise_experience'); ?>
 			<?php echo $form->textfield($experiencePro,'entreprise_experience',array('class'=>'champ-input')); ?>
 			<?php echo $form->error($experiencePro,'entreprise_experience'); ?>
 		</div>
 
-		<div class="rat" id="debut-experience">
+		<div class="row div-field" id="debut-experience">
 			<?php echo $form->labelEx($experiencePro,'date_debut_experience'); ?>
 			<?php echo $form->textfield($experiencePro,'date_debut_experience',array('class'=>'champ-input')); ?>
 			<?php echo $form->error($experiencePro,'date_debut_experience'); ?>
 		</div>
 
-		<div class="rat" id="fin-experience">
+		<div class="row div-field" id="fin-experience">
 			<?php echo $form->labelEx($experiencePro,'date_fin_experience'); ?>
 			<?php echo $form->textfield($experiencePro,'date_fin_experience',array('class'=>'champ-input')); ?>
 			<?php echo $form->error($experiencePro,'date_fin_experience'); ?>
 		</div>
 
-		<div class="rat" id="description-experience">
+		<div class="row div-field" id="description-experience">
 			<?php echo $form->labelEx($experiencePro,'description_experience'); ?>
 			<?php echo $form->textarea($experiencePro,'description_experience',array('class'=>'champ-input champ-description')); ?>
 			<?php echo $form->error($experiencePro,'description_experience'); ?>
@@ -92,24 +92,24 @@
 
 
 		<h1 class='intitule'>Competences</h1>
+
 		
-		<div class="rat" id="intitule-competence">
-			<?php echo $form->labelEx($competence,'intitule_competence'); ?>
-			<?php echo $form->textfield($competence,'intitule_competence',array('class'=>'champ-input')); ?>
+		<div class="row div-field" id="competence">
+			<?php echo $form->textfield($competence,'intitule_competence',array('placeholder'=>'Intitule de la competence', 'size'=>30)); ?>
 			<?php echo $form->error($competence,'intitule_competence'); ?>
-		</div>
+		
 
-
-		<div class="rat" id="niveau-competence">
-			<?php echo $form->radioButtonList($competence,'niveau_competence',array('1','2','3','4','5'),array('separator' => ' ','type'=>'radio')); ?>
+		<?php echo $form->radioButtonList($competence,'niveau_competence',array('1','2','3','4','5'),array('separator' => ' ','class'=>'buttonlist')); ?>
 			<?php echo $form->error($competence,'niveau_competence'); ?>
-		</div>
-		<div class="rat buttons">
-			<?php echo CHtml::submitbutton("Ajouter une competence",array('name' => 'btnajoutcompetence')); ?>
+
 		</div>
 
-		<div class="rat buttons">
-			<?php echo CHtml::submitbutton("Valider",array('name' => 'valider')); ?>
+		<div class="row buttons">
+			<?php echo CHtml::submitbutton("Ajouter une competence",array('name' => 'btnajoutcompetence','id'=>'btn-ajoutcomp')); ?>
+		</div>
+
+		<div class="row buttons">
+			<?php echo CHtml::submitbutton("Valider",array('name' => 'valider','id'=>'btn-valider')); ?>
 		</div>
 
 		<?php $this->endWidget(); ?>
