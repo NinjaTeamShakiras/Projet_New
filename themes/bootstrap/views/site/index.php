a5f9250edf54a008013bfd7315b9b8e4371e3d3b
vanhovea <alexis.vanhove@etu.univ-savoie.fr>
2017-05-15 09:55:08 +0200

    Suite du design

--- themes/bootstrap/views/site/index.php
+++ themes/bootstrap/views/site/index.php
@@ -11,14 +11,16 @@ $form=$this->beginWidget('CActiveForm', array(
 	'action'=>Yii::app()->createUrl('site/Accueil'),
 )); 
 ?>
-<div class="row col-md-">
+<div class="twobuttons row ">
 <?php
 echo CHtml::submitButton('Un emploi/Un stage', array('name' => 'btnemploi','class' => 'btnnoir btn btn-success col-md-offset-2 col-md-2 ')); 
-echo CHtml::submitButton('Un salarié', array('name' => 'btnemploye','class' => 'btnnoir btn btn-success col-md-offset-4 col-md-2 col-md-offset-4 ')); 
+echo CHtml::submitButton('Un salarié', array('name' => 'btnemploye','class' => 'btnnoir btn btn-success col-md-offset-4 col-md-2')); 
 ?>
 </div>
 
-<div class='connexion'>Déjà un compte? <?php echo CHtml::link('Connexion',array('site/login'),array('class'=>'btnredirect')) ?>
+<div class='connexion'>Déjà un compte? <?php echo CHtml::link('Connexion',array('site/login'),array('class'=>'btnredirect'));
+?>
+</div>
 <?php
 $this->endWidget(); 	
 ?>
