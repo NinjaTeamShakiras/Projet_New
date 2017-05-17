4aead6b4d563de060c0cb5fb01d8ced604e94f26
vanhovea <alexis.vanhove@etu.univ-savoie.fr>
2017-05-17 18:23:55 +0200

    Suite du design

---
+++ themes/bootstrap/views/offreEmploi/_search.php
@@ -0,0 +1,78 @@
+<?php
+/* @var $this OffreEmploiController */
+/* @var $model OffreEmploi */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<?php
+	/*
+	<div class="row">
+		<?php echo $form->label($model,'id_offre_emploi'); ?>
+		<?php echo $form->textField($model,'id_offre_emploi'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'date_creation_offre_emploi'); ?>
+		<?php echo $form->textField($model,'date_creation_offre_emploi'); ?>
+	</div>
+
+	*/
+	?>
+
+	<div class="row">
+		<?php echo $form->label($model,'poste_offre_emploi'); ?>
+		<?php echo $form->textField($model,'poste_offre_emploi',array('size'=>45,'maxlength'=>45)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'type_offre_emploi'); ?>
+		<?php
+			// Liste déroulante pour le choix du type d'offre
+			echo $form->dropDownList($model, 'type_offre_emploi', array('CDD'=>'CDD', 'CDI'=>'CDI', 'STAGE'=>'STAGE', 'ALTERNANCE'=>'ALTERNANCE', 'EXTRA'=>'EXTRA'));
+		?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'date_debut_offre_emploi'); ?>
+		<?php echo $form->textField($model,'date_debut_offre_emploi'); ?>
+	</div>
+
+
+	<?php
+	/*
+	<div class="row">
+		<?php echo $form->label($model,'salaire_offre_emploi'); ?>
+		<?php echo $form->textField($model,'salaire_offre_emploi'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'experience_offre_emploi'); ?>
+		<?php echo $form->textField($model,'experience_offre_emploi',array('size'=>60,'maxlength'=>500)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'description_offre_emploi'); ?>
+		<?php echo $form->textField($model,'description_offre_emploi',array('size'=>60,'maxlength'=>500)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'id_entreprise'); ?>
+		<?php echo $form->textField($model,'id_entreprise'); ?>
+	</div>
+	*/
+	?>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
---
+++ themes/bootstrap/views/site/inscriptionEntreprise.php
@@ -0,0 +1,44 @@
+<?php
+$form=$this->beginWidget('CActiveForm',array(
+	'id'=>'inscription-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+));
+
+$entreprise = Entreprise::model();
+?>
+
+	<h1>Inscription</h1>
+
+
+		<div class="row" id="nom_ent">
+			<?php echo $form->textfield($entreprise,'nom_entreprise', array('placeholder'=>'Nom de votre entreprise', 'size'=>50)); ?>
+			<?php echo $form->error($entreprise,'nom_entreprise')."</p>"; ?>
+		</div>
+
+		<div class="row" id="login">
+			<?php echo "<p>".$form->textfield($model,'mail', array('placeholder'=>'Adresse mail', 'size'=>50)); ?>
+			<?php echo $form->error($model,'mail')."</p>"; ?>
+		</div>
+
+		<div class="row" id="mdp">
+			<?php echo "<p>".$form->passwordfield($model,'mot_de_passe', array('placeholder'=>'Mot de passe', 'size'=>25)); ?>
+			<?php echo $form->error($model,'mot_de_passe')."</p>"; ?>
+		</div>
+
+		<div class="row" id="confirm_mdp">
+			<p><input id="Utilisateur_confirm_mdp" type="password" name="confirm_mdp" placeholder="Confirmer le mot de passe" size="25"/></p>
+		</div>
+
+		<div class="row buttons" id="bouton_valid">
+			<?php echo "<p>".CHtml::submitButton('Inscription')."</p>"; ?>
+		</div>
+
+		<div class="row">
+			<?php echo "<p>Vous êtes dêjà inscrit sur le site? ".Chtml::link('Cliquez ici pour vous connectez', array('site/login'))."</p>"; ?>
+		</div>	 
+
+    
+<?php $this->endWidget(); ?>
