f7ad3a62ef071aacf611824135d3d3ccf112fa24
juanpii <bidalia1100@hotmail.com>
2017-05-17 17:49:01 +0200

    Spinner upload CV

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -14,7 +14,7 @@
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
-
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/inscription_employe_cv.css">
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
