ade3b7cfe4bff7b2b0d9b4d97f4c5bd54614e169
vanhovea <alexis.vanhove@etu.univ-savoie.fr>
2017-05-17 14:20:09 +0200

    Suite du design

--- themes/bootstrap/views/site/index.php
+++ themes/bootstrap/views/site/index.php
@@ -1,10 +1,12 @@
 <?php
 /* @var $this SiteController */
+unset(Yii::app()->session['login']);
 echo '<div class=arriere-plan>';
-echo '<div class=filtre-vert>';
-echo '<div class="logo"><img src="images/Prozzl.png" alt="Prozzl"/></div>';
+	echo '<div class=filtre-vert>';
+		echo '<div class="logo"><img src="images/Prozzl.png" alt="Prozzl"/></div>';
 
 //$this->renderPartial('./../entreprise/_search_accueil', array('model'=>Entreprise::model()));
+echo Yii::app()->user->getFlash('suppr_compte');
 
 echo "<div class='Recherche'>Vous recherchez ? </div>";
 
@@ -14,16 +16,19 @@ $form=$this->beginWidget('CActiveForm', array(
 ?>
 <div class="twobuttons row ">
 <?php
-echo CHtml::submitButton('Un emploi/Un stage', array('name' => 'btnemploi','class' => 'btn btn-emploi col-md-offset-3 col-md-2 col-xs-offset-1 col-xs-2')); 
-echo CHtml::submitButton('Un salarié', array('name' => 'btnemploye','class' => 'btn btn-employe col-md-offset-2 col-xs-offset-2 ')); 
+echo CHtml::submitButton('Un emploi/Un stage', array('name' => 'btnemploye','class' => 'btn btn-emploi col-md-offset-3 col-md-2 col-xs-offset-1 col-xs-2')); 
+echo CHtml::submitButton('Un salarié', array('name' => 'btnentreprise','class' => 'btn btn-employe col-md-offset-2 col-xs-offset-2 ')); 
 ?>
 </div>
-
-<div class='connexion'>Déjà un compte? <?php echo CHtml::link('Connexion',array('site/login'),array('class'=>'btnredirect'));
-?>
+<?php
+	if(Yii::app()->user->isGuest)
+	{
+	?>
+<div class='connexion'>Déjà un compte? <?php echo CHtml::link('Connexion',array('site/login'),array('class'=>'btnredirect')); ?>
 </div>	
 </div>
 </div>
 <?php
+}
 $this->endWidget(); 	
 ?>
