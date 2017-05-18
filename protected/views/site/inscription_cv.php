3cd745ead02adb8ebba04880f8c6da78d9c5e664
juanpii <bidalia1100@hotmail.com>
2017-05-18 18:28:49 +0200

    design

--- protected/views/site/inscription_cv.php
+++ protected/views/site/inscription_cv.php
@@ -16,7 +16,7 @@ if($utilisateur != null)
 				Menu 
 				<span class="caret"></span>
 			</button>
-			<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+			<ul id="menu-upload" class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
 				<li>
 					<a href="index.php?r=employe/view&id=<?php echo $utilisateur->id_employe;?>" title="Mon profil">
 							Mon profil
@@ -67,76 +67,82 @@ else
 }
 
 ?>
-<h1>Inscription à partir du CV</h1>
+<a href="./"><img src="/prozzl/images/prozzl_logo.png" alt="Image accueil"></a>
 
-<?php
-	/* --- Sert pour lire le PDF --- */
-	require './protected/vendor/autoload.php';
-	
 
-	/* -- Si le CV a été téléchargé par l'utilisateur -- */
-	if( isset( $_GET[ 'token' ] ) )
-	{
-		$informationsCV_arr = array();
-		/* -- On récupère le texte du CV de l'employe -- */
-		$parser = new \Smalot\PdfParser\Parser();
-		/* -- URL pour récupérer le cv temporaire -- */
-		$url_str = './upload/session/'. $_GET[ 'token' ] . '/cv_' . $_GET[ 'token' ] . '.pdf';
+<div class="div-back-upload-cv">
+	<h1 class="cv-upload-h1">Inscription à partir du CV</h1>
+
+	<?php
+		/* --- Sert pour lire le PDF --- */
+		require './protected/vendor/autoload.php';
 		
-		/* -- Si c'est un fichier valide -- */
-		if( file_exists( $url_str ) )
+
+		/* -- Si le CV a été téléchargé par l'utilisateur -- */
+		if( isset( $_GET[ 'token' ] ) )
 		{
-			/* -- Parse du fichier PDF -- */
-			$CV_pdf = $parser->parseFile( $url_str );
-			/* -- Récupération du texte du PDF -- */
-			$PDFText_str = $CV_pdf->getText();
+			$informationsCV_arr = array();
+			/* -- On récupère le texte du CV de l'employe -- */
+			$parser = new \Smalot\PdfParser\Parser();
+			/* -- URL pour récupérer le cv temporaire -- */
+			$url_str = './upload/session/'. $_GET[ 'token' ] . '/cv_' . $_GET[ 'token' ] . '.pdf';
 			
-			/* -- On teste que la récupération du texte à réussi -- */
-			if (strlen( $PDFText_str ) > 100 )
+			/* -- Si c'est un fichier valide -- */
+			if( file_exists( $url_str ) )
 			{
-				echo '<div style="padding : 2% 0%; text-align: center; widht: 100%;"><b>Extraction des informations réussie</b></div>';
-				$informationsCV_arr = AIPDF::start_algorithm( $CV_pdf );
-				$this->renderPartial( './../site/inscriptionEmployeCV', array( 'informations_arr' => $informationsCV_arr ) );
+				/* -- Parse du fichier PDF -- */
+				$CV_pdf = $parser->parseFile( $url_str );
+				/* -- Récupération du texte du PDF -- */
+				$PDFText_str = $CV_pdf->getText();
+				
+				/* -- On teste que la récupération du texte à réussi -- */
+				if (strlen( $PDFText_str ) > 100 )
+				{
+					echo '<div style="padding : 2% 0%; text-align: center; widht: 100%;"><b>Extraction des informations réussie</b></div>';
+					$informationsCV_arr = AIPDF::start_algorithm( $CV_pdf );
+					$this->renderPartial( './../site/inscriptionEmployeCV', array( 'informations_arr' => $informationsCV_arr ) );
+				}
+				else 
+				{
+					echo '<div>Problème de récupération du texte</div>';
+				}
 			}
-			else 
+			else
 			{
-				echo '<div>Problème de récupération du texte</div>';
+				echo '<div>Téléchargez votre CV ici : </div>';
+				$this->renderPartial( './../employe/_upload_cv', array( 'model' => Employe::model() ) );
 			}
 		}
+		/* -- S'il ne l'a pas téléchargé -- */
 		else
 		{
-			echo '<div>Téléchargez votre CV ici : </div>';
 			$this->renderPartial( './../employe/_upload_cv', array( 'model' => Employe::model() ) );
 		}
-	}
-	/* -- S'il ne l'a pas téléchargé -- */
-	else
-	{
-		$this->renderPartial( './../employe/_upload_cv', array( 'model' => Employe::model() ) );
-	}
 
 
 
-	if($utilisateur == null )
-	{
-		?>
-		<!-- Formulaire avec le bouton pour Inscription -->
-		<div class="form">
-			<?php
-			//Début du form
-			$form=$this->beginWidget('CActiveForm',
-				array(
-					'action'=>Yii::app()->createUrl('/employe/ajoutInfos'),
-				)
-			);
+		if($utilisateur == null )
+		{
 			?>
+			<!-- Formulaire avec le bouton pour Inscription -->
+			<div class="form">
+				<?php
+				//Début du form
+				$form=$this->beginWidget('CActiveForm',
+					array(
+						'action'=>Yii::app()->createUrl('/employe/ajoutInfos'),
+					)
+				);
+				?>
 
-			<div class="row">
-				<?php echo CHtml::submitButton('Renseigner mes informations personnelles et générer mon CV'); ?>
+				<div class="row">
+					<?php echo CHtml::submitButton('Renseigner mes informations personnelles et générer mon CV'); ?>
+				</div>
+
+				<?php $this->endWidget(); ?>
+			
 			</div>
-
-			<?php $this->endWidget(); ?>
-		
-		</div>
-		<?php 
-	}
+			<?php 
+		}
+?>
+</div>
